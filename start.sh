617cad38f5e37f3b0786e815e73f874f62c3d4c5
Suneel <suneel.peruru@gmail.com>
2026-03-16 17:37:47 +0530

    Update paths in start.sh for local dependencies

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pip3 install --no-cache-dir --target /app/deps -r /app/requirements.txt
-PYTHONPATH=/app/deps python3 -m uvicorn app:app --host 0.0.0.0 --port 8080
+pip3 install --no-cache-dir --target ./deps -r ./requirements.txt
+PYTHONPATH=./deps python3 -m uvicorn app:app --host 0.0.0.0 --port 8080
